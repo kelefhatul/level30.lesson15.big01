01cb4d9dec3ffee48af2f3ec491d62e55ffbbbb5
kelefhatul <kostyantinromanov@gmail.com>
2016-11-19 15:38:24 +0200

    task13

--- big01/client/Client.java
+++ big01/client/Client.java
@@ -1,6 +1,11 @@
 package com.javarush.test.level30.lesson15.big01.client;
 
 import com.javarush.test.level30.lesson15.big01.Connection;
+import com.javarush.test.level30.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level30.lesson15.big01.Message;
+import com.javarush.test.level30.lesson15.big01.MessageType;
+
+import java.io.IOException;
 
 /**
  * Created by root on 11/19/2016.
@@ -11,5 +16,39 @@ public class Client {
 
     public class SocketThread extends Thread{
 
+    }
+
+    protected String getServerAddress(){
+        ConsoleHelper.writeMessage("Input server IP:");
+        String IP = ConsoleHelper.readString();
+        return IP;
+    }
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Input server port:");
+        int serverPort = ConsoleHelper.readInt();
+        return serverPort;
+    }
+
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Input user name: ");
+        String userName = ConsoleHelper.readString();
+        return userName;
+    }
+
+    protected boolean shouldSentTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT,text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Can not send message, connection problem");
+            clientConnected = false;
+        }
     }
 }
