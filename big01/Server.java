1ee9902c363fcc87b900cb57d06ba7d8c5cbb515
kelefhatul <kostyantinromanov@gmail.com>
2016-11-19 13:29:39 +0200

    task07

--- big01/Server.java
+++ big01/Server.java
@@ -3,17 +3,34 @@ package com.javarush.test.level30.lesson15.big01;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by root on 11/18/2016.
  */
 public class Server {
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     private static class Handler extends Thread{
 
         private Socket socket;
 
         public Handler(Socket socket){
             this.socket = socket;
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message){
+        for (Map.Entry<String, Connection> pair : connectionMap.entrySet()
+             ) {
+            try {
+                pair.getValue().send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Can not send a message");
+//                e.printStackTrace();
+            }
         }
     }
 
