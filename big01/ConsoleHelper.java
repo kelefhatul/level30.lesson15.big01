1e66b60f6024a71bbfcb64e8fff5b50cfc7efc3e
kelefhatul <kostyantinromanov@gmail.com>
2016-11-19 12:17:50 +0200

    task02

--- big01/ConsoleHelper.java
+++ big01/ConsoleHelper.java
@@ -1,7 +1,42 @@
 package com.javarush.test.level30.lesson15.big01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by root on 11/18/2016.
  */
 public class ConsoleHelper {
+    private static BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+
+    public static String readString(){
+        String inData = "";
+        while (true){
+            try {
+                inData = keyboard.readLine();
+                break;
+            } catch (IOException e) {
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            } 
+        }
+        return inData;
+    }
+
+    public static int readInt(){
+        int result = 0;
+        while (true){
+            try {
+                result = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException e) {
+                writeMessage("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+        return result;
+    }
 }
